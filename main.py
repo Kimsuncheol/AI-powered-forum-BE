9f134cb6b941aa7663ebc2e764bdceeb38bd0826
Kimsuncheol <cheonjae6@naver.com>
2025-12-03 00:40:15 +0900

    chore: Initialize virtual environment and install project dependencies.

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import routes_auth, routes_user
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json")
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include routers
 api_router = APIRouter()
